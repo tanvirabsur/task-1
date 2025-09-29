465e3c417422356931df34852cc7911726dcb0e2
tanvirabsar <tjoy4799@gmail.com>
2025-09-29 16:56:38 +0600

    add navbar

---
+++ src/components/FAq.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FAq = () => {
+    return (
+        <div tabIndex={0} className="collapse collapse-arrow bg-base-100 border-base-300 border">
+            <div className="collapse-title font-semibold">How do I create an account?</div>
+            <div className="collapse-content text-sm">
+                Click the "Sign Up" button in the top right corner and follow the registration process.
+            </div>
+        </div>
+    );
+};
+
+export default FAq;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <a className="btn btn-ghost text-xl">Excusive</a>
+        </div>
+    );
+};
+
+export default Navbar;
